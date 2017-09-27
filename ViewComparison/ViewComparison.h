c2c1c08450b6a61bf4a65ebf1ced1a6af24f32d7
Dmytro Anokhin <danokhin@192.168.2.5>
2017-09-27 06:21:36 +0200

    Initial Commit

---
+++ ViewComparison/ViewComparison.h
@@ -0,0 +1,19 @@
+//
+//  ViewComparison.h
+//  ViewComparison
+//
+//  Created by Dmytro Anokhin on 27/09/2017.
+//  Copyright © 2017 Dmytro Anokhin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ViewComparison.
+FOUNDATION_EXPORT double ViewComparisonVersionNumber;
+
+//! Project version string for ViewComparison.
+FOUNDATION_EXPORT const unsigned char ViewComparisonVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ViewComparison/PublicHeader.h>
+
+
